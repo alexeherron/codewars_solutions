6dc240af01b7a500131b84d5db2a71e1f286123b
alexherron <alexeherron@gmail.com>
2022-12-15 09:31:24 -0600

    Adding new codewars solutions

--- 8kyu/studentfinalgrade.js
+++ 8kyu/studentfinalgrade.js
@@ -9,25 +9,3 @@
 //     75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
 //     0, in other cases
 
-
-//MY SOLUTION
-function finalGrade(exam, projects){
-    if(exam > 90 || projects > 10){
-      return 100;
-    }else if(exam > 75 && projects >= 5){
-      return 90;
-    }else if(exam > 50 && projects >=2){
-      return 75;
-    }else{
-      return 0;
-    }
-  }
-  
-  //OTHER CLEVER SOLUTION
-  const finalGrade = (exam, projects) => {
-    return (
-      exam > 90 || projects > 10 ? 100 :
-      exam > 75 && projects >= 5 ? 90 :
-      exam > 50 && projects >= 2 ? 75 : 0
-    )
-  }
