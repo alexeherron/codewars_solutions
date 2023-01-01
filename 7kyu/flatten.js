bc40cae3cea060abf67f3b22ec1263c2b9265b9a
alexherron <alexeherron@gmail.com>
2022-12-31 18:05:47 -0600

    "Added new 7kyu solution"

---
+++ 7kyu/flatten.js
@@ -0,0 +1,14 @@
+//FLATTEN
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+//MY SOLUTION
+//const flatten = array => array.flat();
+
+//OTHER CLEVER SOLUTION
+var flatten = function (lol){
+    return [].concat.apply([],lol);
+  }
