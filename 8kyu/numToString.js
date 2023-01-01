2b2148c7d19fba71831a3dee51a3e02affc277be
alexherron <alexeherron@gmail.com>
2022-12-31 18:00:03 -0600

    "Added new codewars solution"

---
+++ 8kyu/numToString.js
@@ -0,0 +1,15 @@
+//NUMBER TO STRING
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+//MY SOLUTION
+const numToString = num => String(num);
+
+//OTHER SOLUTION
+const numberToString = num => `${num}`;
