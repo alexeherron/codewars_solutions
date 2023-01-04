90125453f66e704a255253c315f67f5bbf689dc3
alexherron <alexeherron@gmail.com>
2023-01-04 08:41:10 -0600

    Added new 6kyu codewars solution

---
+++ 6kyu/whoLikes.js
@@ -0,0 +1,40 @@
+//WHO LIKES IT?
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+//MY SOLUTION
+function likes(array){
+    let num = array.length;
+    if(num == 0){
+        return "no one likes this";
+    }else if(num == 1){
+        return `${array[0]} likes this`
+    }else if(num == 2){
+        return `${array[0]} and ${array[1]} like this`
+    }else if(num == 3){
+        return `${array[0]}, ${array[1]} and ${array[2]} like this`
+    }else{
+        return `${array[0]}, ${array[1]} and ${num - 2} others like this`
+    }
+}
+
+//OTHER CLEVER SOLUTION
+function likes(names) {
+    names = names || [];
+    switch(names.length){
+      case 0: return 'no one likes this'; break;
+      case 1: return names[0] + ' likes this'; break;
+      case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+      case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+      default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    }
+}
