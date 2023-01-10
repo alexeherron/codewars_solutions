41aa522207c112f1d47f3b837a61fb8380251206
alexherron <alexeherron@gmail.com>
2023-01-10 08:43:02 -0600

    Added new codewars solution

---
+++ 8kyu/parseIntFromChar.js
@@ -0,0 +1,12 @@
+//PARSE INT FROM CHAR
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+//MY SOLUTION
+//const getAge = inputString => Number(inputString[0]);
+
+//OTHER CLEVER SOLUTION
+//const getAge = parseInt;
