80e81c6960de95132b3f8a5518a495f2891df2dd
alexherron <alexeherron@gmail.com>
2023-01-21 10:55:02 -0600

    Added new python codewars solution

---
+++ 8kyu/grasshopper_basic_function_fixer.py
@@ -0,0 +1,17 @@
+#GRASSHOPPER - BASIC FUNCTION FIXER
+# Fix the function
+
+# I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+# Can you help me fix the function?
+
+# def add_five(num):
+#     total = num + 5
+#     return num
+
+#MY SOLUTION
+def add_five(num):
+    return num + 5
+
+#OTHER CLEVER SOLUTION
+add_five = (5).__add__
