ef8e8fb49054f094567c4e0f18616cbfad1e34dc
alexherron <alexeherron@gmail.com>
2023-01-21 10:51:45 -0600

    Added new codewars solution

---
+++ 8kyu/grasshopperBasicFuctionFixer.js
@@ -0,0 +1,17 @@
+//GRASSHOPPER - BASIC FUNCTION FIXER
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+// function addFive(num) {
+//     var total = num + 5
+//     return num
+//   }
+
+//MY SOLUTION
+const addFive = num => num + 5
+
+//OTHER CLEVER SOLUTION
+function addFive(num) {
+    return num + 5;
+}
