1f74beb204c3ac1c155d080e2e67d505687759e6
alexherron <alexeherron@gmail.com>
2023-01-22 08:42:12 -0600

    Added new codewars solution

---
+++ 8kyu/messiGoals.js
@@ -0,0 +1,31 @@
+//MESSI GOALS
+// Messi's Goal Total
+
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+// Information
+
+// Messi goal scoring statistics:
+// Competition 	Goals
+// La Liga 	43
+// Champions League 	10
+// Copa del Rey 	5
+// Task
+
+//     Create these three variables and store the appropriate values using the table above:
+
+//     laLigaGoals
+//     championsLeagueGoals
+//     copaDelReyGoals
+
+//     Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+//MY SOLUTION
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+//OTHER CLEVER SOLUTION
+// let goals = [43, 10, 5],
+// [laLigaGoals, championsLeagueGoals, copaDelReyGoals] = goals,
+// totalGoals = goals.reduce((s, v) => s + v);
