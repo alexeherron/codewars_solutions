d301a0d3ce09aa6155b21b3a14c0090b3b737af9
alexherron <alexeherron@gmail.com>
2023-01-26 14:46:34 -0600

    Added new javascript and python codewars solutions

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,12 @@
+//CONVERT A BOOLEAN TO A STRING
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//MY SOLUTION
+const boolToString = bool => bool == true ? 'true' : 'false'
+
+//OTHER CLEVER SOLUTION
+function booleanToString(b){
+  return b.toString();
+}
---
+++ 8kyu/boolean_to_string.py
@@ -0,0 +1,11 @@
+#CONVERT BOOLEAN TO STRING
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+#MY SOLUTION
+def boolean_to_string(b):
+    return str(b)
+
+#OTHER CLEVER SOLUTION
+boolean_to_string = str   
