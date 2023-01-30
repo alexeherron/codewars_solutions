0bd4627a8da0dcd0b3b26ea6e709c425942f4231
alexherron <alexeherron@gmail.com>
2023-01-30 16:42:07 -0600

    Added new python codewars solution

---
+++ 8kyu/hello_world.py
@@ -0,0 +1,18 @@
+#FUNCTION 1 - HELLO WORLD
+#You know what it is
+
+#MY SOLUTION
+def greet():
+    return 'hello world!'
+
+#OTHER CLEVER SOLUTION
+def greet():
+    x=[
+    "Soulja Boy up in this hoe",
+    "watch me crank dat, watch me roll",
+    "watch me crank dat Soulja Boy and superman dat hoe",
+    "now watch me YOOOOOU!",
+    "crank dat Soulja Boy",
+    ]
+    greet=str(x[0][-3]+x[0][-1]+x[0][3]+x[1][-1]+x[0][-2]+" "+x[2][0]+x[0][1]+x[1][10]+x[2][22]+x[4][6]+x[3][-1])
+    return greet
