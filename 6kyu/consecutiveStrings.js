6891012c73e72b5a004dfe4022a3542619f60c62
alexherron <alexeherron@gmail.com>
2023-02-04 10:40:43 -0600

    Added new 6kyu codewars solution

---
+++ 6kyu/consecutiveStrings.js
@@ -0,0 +1,55 @@
+//CONSECUTIVE STRINGS
+// You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+// Examples:
+
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so 
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+// Note
+
+// consecutive strings : follow one after another without an interruption
+
+//MY SOLUTION
+function consecStr(strarr, k){
+    let concats = []
+    for(i = 0; i < strarr.length; i++){
+        let conjoiny = strarr.slice(i, (i+k))
+        conjoiny = conjoiny.join('')
+        concats.push(conjoiny)
+    }
+    let maxLength = Math.max.apply(Math, concats.map(function (el) { return el.length }));
+    let result = concats.find(x => x.length == maxLength)
+    if(k <= 0 || k > strarr.length || result == undefined){
+      return '';
+    }else{
+      return result;
+    }
+}
+
+//OTHER CLEVER SOLUTION
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+        var tempArray = strarr.slice(i,i+k);
+        var tempStr = tempArray.join("");
+        if(tempStr.length > longest.length){
+            longest = tempStr;
+        }
+    }
+    return longest;
+}
