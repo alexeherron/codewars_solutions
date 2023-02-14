e3ccd2859363edd57db2a6b7ca8200fc2dbfd64d
alexherron <alexeherron@gmail.com>
2023-02-14 14:46:39 -0600

    Added new codewars solution

---
+++ 8kyu/duckDuckGoose.js
@@ -0,0 +1,26 @@
+//DUCK DUCK GOOSE
+// The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+// Example:
+
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+
+//MY SOLUTION
+function duckDuckGoose(arr,n){
+    let m = n - 1
+    console.log(m)
+    if(m > arr.length-1){
+        m = ((m/arr.length) - Math.floor(m/arr.length))*arr.length  
+    }
+    return arr[m].name
+}
+
+//OTHER CLEVER SOLUTION
+function duckDuckGoose(players, goose) {
+  
+    return players[(goose-1)%players.length].name
+}
