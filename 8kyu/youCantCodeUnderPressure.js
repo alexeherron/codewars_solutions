91183fa813e6230e553abd93ea263ec7f7c64b28
alexherron <alexeherron@gmail.com>
2023-02-15 08:41:25 -0600

    Added new codewars solution

---
+++ 8kyu/youCantCodeUnderPressure.js
@@ -0,0 +1,10 @@
+//YOU CAN'T CODE UNDER PRESSURE
+// Code as fast as you can! You need to double the integer and return it.
+
+//MY SOLUTION
+const doubleIt = n => 2*n
+
+//OTHER CLEVER SOLUTION
+function doubleInteger(i) {
+    return i << 1;
+}
