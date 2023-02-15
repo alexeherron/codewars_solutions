3d864a4785c5d953486153024484b6529aaa4234
alexherron <alexeherron@gmail.com>
2023-02-15 08:43:45 -0600

    Added new python codewars solution

---
+++ 8kyu/you_cant_code_under_pressure.py
@@ -0,0 +1,10 @@
+#YOU CAN'T CODE UNDER PRESSURE
+#Code as fast as you can! You need to double the integer and return it.
+
+#MY SOLUTION
+def double_integer(i):
+    return i*2
+
+#OTHER CLEVER SOLUTION
+def doubleInteger(i):
+    return i << 1
