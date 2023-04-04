6659c7adeb621a8c105dd3ec05eb60e0fb5ebe25
alexherron <alexeherron@gmail.com>
2023-04-04 08:46:27 -0500

    Added new javascript and python 7kyu codewars solutions

---
+++ 7kyu/between_extremes.py
@@ -0,0 +1,17 @@
+# BETWEEN EXTREMES
+# Given an array of numbers, return the difference between the largest and smallest values.
+
+# For example:
+
+# [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+# [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+# The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+# MY SOLUTION
+def between_extremes(numbers):
+    return max(numbers) - min(numbers)
+
+# OTHER CLEVER SOLUTION
+from numpy import ptp as between_extremes
