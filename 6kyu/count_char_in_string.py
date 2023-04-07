5b591c034d425a4c9549f0b9034c341fd146e197
alexherron <alexeherron@gmail.com>
2023-04-07 08:42:59 -0500

    Added new 6kyu python codewars solution

---
+++ 6kyu/count_char_in_string.py
@@ -0,0 +1,19 @@
+# COUNT CHARACTERS IN YOUR STRING
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+# MY SOLUTION
+def count(s):
+    count = {}
+    for char in s:
+        if char in count.keys():
+            count[char] += 1
+        else:
+            count[char] = 1
+    return count
+
+# OTHER CLEVER SOLUTION
+def count(string):
+  
+    return {i: string.count(i) for i in string}
