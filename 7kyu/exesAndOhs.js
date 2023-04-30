535ecfda034d066a354a8472942d305df2175f69
alexherron <alexeherron@gmail.com>
2023-04-29 22:07:04 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/exesAndOhs.js
@@ -0,0 +1,19 @@
+//EXES AND OHS
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+//MY SOLUTION
+const XO = string => string.split('').filter(x => x == 'x' || x == 'X').length == string.split('').filter(x => x == 'o' || x == 'O').length;
+
+//OTHER CLEVER SOLUTION
+const XO = str => {
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+}
