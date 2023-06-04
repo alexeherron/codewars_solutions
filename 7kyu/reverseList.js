09f1f4fba775c0f7069fcbefe81ef929fc23582e
alexherron <alexeherron@gmail.com>
2023-06-04 07:47:34 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/reverseList.js
@@ -0,0 +1,10 @@
+//REVERSE LIST
+// Write reverseList function that simply reverses lists.
+
+//MY SOLUTION
+const reverseList = arr => arr.reverse();
+
+//OTHER CLEVER SOLUTION
+function reverseList(arr) {
+    return arr.reverse();
+  }
