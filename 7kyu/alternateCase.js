d307a3494af9144d2869cafd25043376cccfcaa2
alexherron <alexeherron@gmail.com>
2023-06-09 13:21:56 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/alternateCase.js
@@ -0,0 +1,12 @@
+//ALTERNATE CASE
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+//MY SOLUTION
+const alternateCase = string => string.split('').map(x => (x == x.toUpperCase()) ? x.toLowerCase() : x.toUpperCase()).join('');
+
+//OTHER CLEVER SOLUTION
+function alternateCase(s) {
+    return s.split('').map(function(el) {
+      return el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase();
+    }).join('');
+}
