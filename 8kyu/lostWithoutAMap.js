b984134101c483c52ad9846700d246901adc71f9
alexherron <alexeherron@gmail.com>
2023-06-14 10:49:07 -0500

    Added new codewars solution

---
+++ 8kyu/lostWithoutAMap.js
@@ -0,0 +1,14 @@
+//BEGINNER - LOST WITHOUT A MAP
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//MY SOLUTION
+const lost = arr => arr.map(x => x*2);
+
+//OTHER CLEVER SOLUTION
+function maps(x){
+    return x.map(n => n * 2);
+}
