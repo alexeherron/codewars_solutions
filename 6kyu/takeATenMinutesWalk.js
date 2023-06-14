aa2e710409f936f6a3ac5593e584bcb1af1c76d8
alexherron <alexeherron@gmail.com>
2023-06-14 08:51:39 -0500

    Added a new 6kyu codewars solution

---
+++ 6kyu/takeATenMinutesWalk.js
@@ -0,0 +1,33 @@
+//TAKE A TEN MINUTES WALK
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+//     Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+//MY SOLUTION
+function tenMin(arr){
+    let lat = 0;
+    let lon = 0;
+    lat += arr.filter(x => x == 'n').length;
+    lat -= arr.filter(x => x == 's').length;
+    lon += arr.filter(x => x == 'e').length;
+    lon -= arr.filter(x => x == 'w').length;
+    return ((arr.length == 10) && (lat == 0) && (lon == 0));
+}
+
+//OTHER CLEVER SOLUTION
+function isValidWalk(walk) {
+    var dx = 0
+    var dy = 0
+    var dt = walk.length
+    
+    for (var i = 0; i < walk.length; i++) {
+      switch (walk[i]) {
+        case 'n': dy--; break
+        case 's': dy++; break
+        case 'w': dx--; break
+        case 'e': dx++; break
+      }
+    }
+    
+    return dt === 10 && dx === 0 && dy === 0
+}
