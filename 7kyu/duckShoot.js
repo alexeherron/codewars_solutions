9d1a47713c61af3458dbdf8d7039a348bf4b46ee
alexherron <alexeherron@gmail.com>
2023-06-30 14:39:38 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/duckShoot.js
@@ -0,0 +1,33 @@
+//DUCK SHOOT - EASY
+// You've arrived at a carnival and head straight for the duck shooting tent. Why wouldn't you?
+
+// You will be given a set amount of ammo, and an aim rating of between 1 and 0. No your aim is not always perfect - hey maybe someone fiddled with the sights on the gun...
+
+// Anyway your task is to calculate how many successful shots you will be able to make given the available ammo and your aim score, then return a string representing the pool of ducks, with those ducks shot marked with 'X' and those that survived left unchanged. You will always shoot left to right.
+
+// Example of start and end duck string with two successful shots:
+
+// Start ---> |~~~~~22~2~~~~~|
+
+// Bang!! Bang!!
+
+// End ---> |~~~~~XX~2~~~~~|
+
+// All inputs will be correct type and never empty.
+
+//MY SOLUTION
+function duckShoot(ammo, aim, ducks){
+    let heDed = Math.floor(ammo*aim);
+    for(i = 0; i < heDed; i++){
+      ducks = ducks.replace('2', 'X');
+    }
+    return ducks;
+}
+
+//OTHER CLEVER SOLUTION
+function duckShoot(ammo, aim, ducks){
+    for (var i=0; i<Math.floor(ammo * aim); i++) {
+      ducks = ducks.replace("2", "X");
+    }
+    return ducks;
+}
