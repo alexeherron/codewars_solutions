6341792776dfdb0fbec2a0fe805fc0f3f19587b5
alexherron <alexeherron@gmail.com>
2023-08-17 08:32:08 -0500

    Added new codewars solution

---
+++ 8kyu/nameOnBillboard.js
@@ -0,0 +1,28 @@
+//NAME ON BILLBOARD
+// You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+
+// You can not use multiplier "*" operator.
+
+// If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
+
+//MY SOLUTION
+// function nameOnBillboard(str){
+//     let price = 0;
+//     for(i = 0; i < str.length; i++){
+//         price += 30;
+//     }
+//     return price;
+// }
+const nameOnBillboard = (str, price) => str.split('').map(x => price || 30).reduce((acc, item) => acc + item, 0);
+
+//OTHER CLEVER SOLUTION
+function billboard(name, price = 30){
+
+    var totalCost = 0;
+    for(i=0; i<name.length; i++){
+        totalCost += price;
+    } 
+    
+    return totalCost;
+    
+    }
