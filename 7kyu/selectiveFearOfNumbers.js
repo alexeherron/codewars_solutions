23ee96e75b771fcca8ff6395d3362bf5e4ee9188
alexherron <alexeherron@gmail.com>
2023-08-19 19:10:57 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/selectiveFearOfNumbers.js
@@ -0,0 +1,41 @@
+//SELECTIVE FEAR OF NUMBERS
+// I've got a crazy mental illness. I dislike numbers a lot. But it's a little complicated: The number I'm afraid of depends on which day of the week it is... This is a concrete description of my mental illness:
+
+// Monday --> 12
+
+// Tuesday --> numbers greater than 95
+
+// Wednesday --> 34
+
+// Thursday --> 0
+
+// Friday --> numbers divisible by 2
+
+// Saturday --> 56
+
+// Sunday --> 666 or -666
+
+// Write a function which takes a string (day of the week) and an integer (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
+
+//MY SOLUTION
+const macroData = (day, num) => {
+    return (day == 'Monday' && num == 12) ? true : 
+    (day == 'Tuesday' && num > 95) ? true : 
+    (day == 'Wednesday' && num == 34) ? true : 
+    (day == 'Thursday' && num == 0) ? true : 
+    (day == 'Friday' && num % 2 == 0) ? true : 
+    (day == 'Saturday' && num == 56) ? true : 
+    (day == 'Sunday' && (num == 666 || num == -666)) ? true : false;
+}
+
+//OTHER CLEVER SOLUTION
+var AmIAfraid = function(day, num) {
+    var preds = {"Sunday":    d => d == 666 || d == -666,
+                 "Monday":    d => d == 12,
+                 "Tuesday":   d => d > 95,
+                 "Wednesday": d => d == 34,
+                 "Thursday":  d => d === 0,
+                 "Friday":    d => d % 2 === 0,
+                 "Saturday":  d => d == 56};
+    return preds[day](num);
+}
