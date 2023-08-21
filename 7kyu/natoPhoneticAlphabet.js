63b472fbe88e77f2ac33fcdecbbe147af107d4f2
alexherron <alexeherron@gmail.com>
2023-08-21 10:51:29 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/natoPhoneticAlphabet.js
@@ -0,0 +1,50 @@
+//NATO PHONETIC ALPHABET
+// Complete the function word (string) and returns a string that spells the word using the NATO phonetic alphabet.
+
+// There should be a space between each word in the returned string, and the first letter of each word should be capitalized.
+
+// For those of you that don't want your fingers to bleed, this kata already has a dictionary typed out for you.
+// Examples
+
+// "hi"      -->  "Hotel India"
+// "abc"     -->  "Alpha Bravo Charlie"
+// "babble"  -->  "Bravo Alpha Bravo Bravo Lima Echo"
+// "Banana"  -->  "Bravo Alpha November Alpha November Alpha"
+
+//MY SOLUTION
+const nato = str => {
+    const letters =  {
+        "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+        "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+        "G": "Golf",   "H": "Hotel",   "I": "India",
+        "J": "Juliett","K": "Kilo",    "L": "Lima",
+        "M": "Mike",   "N": "November","O": "Oscar",
+        "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+        "S": "Sierra", "T": "Tango",   "U": "Uniform",
+        "V": "Victor", "W": "Whiskey", "X": "X-ray",
+        "Y": "Yankee", "Z": "Zulu"
+      }
+
+    return str.toUpperCase().split('').map(x => letters[x]).join(' ');
+}
+
+//OTHER CLEVER SOLUTION
+var nato = (function() {
+    var letters =  {
+      "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+      "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+      "G": "Golf",   "H": "Hotel",   "I": "India",
+      "J": "Juliett","K": "Kilo",    "L": "Lima",
+      "M": "Mike",   "N": "November","O": "Oscar",
+      "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+      "S": "Sierra", "T": "Tango",   "U": "Uniform",
+      "V": "Victor", "W": "Whiskey", "X": "X-ray",
+      "Y": "Yankee", "Z": "Zulu"
+    }
+    
+    return function(word) {
+      return word.split('').map(function(v) {
+        return letters[v.toUpperCase()]
+      }).join(' ');
+    }
+  })()
