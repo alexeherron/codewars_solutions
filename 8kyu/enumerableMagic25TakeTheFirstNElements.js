3a04e683e2faedbb268201e14fe0aabf8b0a9241
alexherron <alexeherron@gmail.com>
2023-09-09 11:52:34 -0500

    Added new codewars solution

---
+++ 8kyu/enumerableMagic25TakeTheFirstNElements.js
@@ -0,0 +1,8 @@
+//ENUMERABLE MAGIC #25 - TAKE THE FIRST N ELEMENTS
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+//MY SOLUTION
+const firstN = (arr,n) => arr.slice(n);
+
+//OTHER CLEVER SOLUTION
+const take = (arr,n) => arr.slice(n);
