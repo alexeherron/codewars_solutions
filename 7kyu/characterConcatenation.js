954ec129bb3f498cfb0e33f4dcbc2420d38fa22a
alexherron <alexeherron@gmail.com>
2023-09-14 08:35:24 -0500

    Added new 7kyu codewars solution

---
+++ 7kyu/characterConcatenation.js
@@ -0,0 +1,32 @@
+//CHARACTER CONCATENATION
+// Given a string, you progressively need to concatenate the first letter from the left and the first letter to the right and "1", then the second letter from the left and the second letter to the right and "2", and so on.
+
+// If the string's length is odd drop the central element.
+
+// For example:
+
+// charConcat("abcdef")    == 'af1be2cd3'
+// charConcat("abc!def")   == 'af1be2cd3' // same result
+
+//MY SOLUTION
+function charConcat(str){
+    let first = str.split('').slice(0,Math.floor(str.length/2));
+    let second = str.split('').reverse().slice(0,Math.floor(str.length/2));
+    let result = '';
+    for(i = 0; i < first.length; i++){
+        result += `${first[i]}${second[i]}${i+1}`;
+    }
+    return result;
+}
+
+//OTHER CLEVER SOLUTION
+function charConcat(str) {
+    var i=1;
+    var res='';
+    while(str.length>1){
+        res+=str.charAt(0)+str.charAt(str.length-1)+i;
+      i++;
+      str=str.slice(1,-1)
+    }
+    return res;
+}
