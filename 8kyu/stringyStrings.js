a58ff5c985f05fe88630e09ad209f349da9b6cf6
alexherron <alexeherron@gmail.com>
2023-09-27 10:03:50 -0500

    Added new codewars solution

---
+++ 8kyu/stringyStrings.js
@@ -0,0 +1,23 @@
+//STRINGY STRINGS
+// write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+
+// the string should start with a 1.
+
+// a string with size 6 should return :'101010'.
+
+// with size 4 should return : '1010'.
+
+// with size 12 should return : '101010101010'.
+
+// The size will always be positive and will only use whole numbers.
+
+//MY SOLUTION
+const stringy = num => Array(num).fill('1').map((x,i) => (i % 2 != 0) ? '0' : x).join('');
+
+//OTHER CLEVER SOLUTION
+function stringy(size) {
+    var str='';
+    for( var i=1; i<=size; i++ )
+      str+=i%2;
+    return str;
+}
