432771ff91062a757cf47e66591fa53292476f14
alexherron <alexeherron@gmail.com>
2023-11-13 11:16:30 -0600

    Added new 6kyu codewars solution

---
+++ 6kyu/fizzBuzzBackwards.js
@@ -0,0 +1,48 @@
+// FIZZBUZZ BACKWARDS
+// Traditionally in FizzBuzz, multiples of 3 are replaced by "Fizz" and multiples of 5 are replaced by "Buzz". But we could also play FizzBuzz with any other integer pair [n, m] whose multiples are replaced with Fizz and Buzz.
+
+// For a sequence of numbers, Fizzes, Buzzes and FizzBuzzes, find the numbers whose multiples are being replaced by Fizz and Buzz. Return them as an array [n, m]
+
+// The Fizz and Buzz numbers will always be integers between 1 and 50, and the sequence will have a maximum length of 100. The Fizz and Buzz numbers might be equal, and might be equal to 1.
+// Examples
+
+//     Classic FizzBuzz; multiples of 3 are replaced by Fizz, multiples of 5 are replaced by Buzz:
+
+//     [1, 2, "Fizz", 4, "Buzz", 6]  ==>  [3, 5] 
+//     Multiples of 2 are replaced by Fizz, multiples of 3 are replaced by Buzz:
+//     [1, "Fizz", "Buzz", "Fizz", 5, "FizzBuzz"]  ==>  [2, 3]
+//     Multiples of 2 are replaced by Fizz and Buzz:
+//     [1, "FizzBuzz", 3, "FizzBuzz", 5, "FizzBuzz"]  ==>  [2, 2]
+//     Fizz = 1, Buzz = 6:
+//     ["Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "FizzBuzz"]  ==>  [1, 6]
+
+// MY SOLUTION
+function fizzbuzzBackwards(arr){
+    let fizzBuzz = (arr.indexOf('FizzBuzz') + 1);
+    let fizz = (arr.indexOf('Fizz') > -1) ? (arr.indexOf('Fizz') + 1) : null;
+    let buzz = (arr.indexOf('Buzz') > -1) ? (arr.indexOf('Buzz') + 1) : null;
+    if(fizz && buzz){
+        return [fizz, buzz];
+    }else if(!fizz && !buzz){
+        return [fizzBuzz, fizzBuzz];
+    }else{
+        if(!fizz){
+          if(fizzBuzz % buzz == 0){
+            fizz = fizzBuzz;
+          }
+          return [fizz,buzz];
+        }else if(!buzz){
+            if(fizzBuzz % fizz == 0){
+            buzz = fizzBuzz;
+          }
+        return [fizz,buzz];
+    }}}
+
+// OTHER CLEVER SOLUTION
+function reverseFizzBuzz(array) {
+    var fizz = (array.indexOf("Fizz") + 1) ? array.indexOf("Fizz") + 1 : array.indexOf("FizzBuzz")+1;
+    var buzz = (array.indexOf("Buzz") + 1) ? array.indexOf("Buzz") + 1 : array.indexOf("FizzBuzz")+1;
+  
+  return [fizz,buzz];
+  
+};
