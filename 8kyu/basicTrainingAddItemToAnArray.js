1de897a3fd6e46bcc47109a1ed9a4ee4188f534e
alexeherron <alexeherron@gmail.com>
2024-01-19 07:55:35 -0600

    Added new codewars solution

---
+++ 8kyu/basicTrainingAddItemToAnArray.js
@@ -0,0 +1,14 @@
+// BASIC TRAINING: ADD ITEM TO AN ARRAY
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+// MY SOLUTION
+const basicTraining = arr => arr.push('codewars');
+basicTraining(websites);
+
+// OTHER CLEVER SOLUTION
+websites.push('codewars');
